82b0f62d830ed0f3f50efef090b3b57b926cc43c
likithabm14 <likithabm57@gmail.com>
2026-06-04 15:59:47 +0530

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -2,15 +2,8 @@ import pandas as pd
 import streamlit as st
 
 @st.cache_data
-def load_data(file):
-
-    try:
-        df = pd.read_csv(file)
-
-        df.drop_duplicates(inplace=True)
-
-        return df
-
-    except Exception as e:
-        st.error(f"Error Loading Dataset: {e}")
-        return pd.DataFrame()
+def load_data():
+    df = pd.read_csv(
+        "data/corporate_ai_adoption_dataset.csv"
+    )
+    return df

5b9e8f4d92916536e61987cda0a22370e915d0eb
likithabm14 <likithabm57@gmail.com>
2026-06-04 16:33:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 import streamlit as st
+
 from utils.data_loader import load_data
+from utils.charts import (
+    adoption_trend_chart,
+    industry_chart,
+    country_chart,
+    investment_vs_revenue_chart
+)
+from utils.insights import generate_insights
 
 st.set_page_config(
     page_title="Corporate AI Dashboard",
@@ -7,22 +15,50 @@ st.set_page_config(
     layout="wide"
 )
 
-st.title("🤖 Corporate AI Adoption Analytics Dashboard")
+st.title("🤖 Corporate AI Adoption Dashboard")
 
-uploaded_file = st.sidebar.file_uploader(
-    "Upload CSV Dataset",
-    type=["csv"]
-)
-
-if uploaded_file is None:
-    st.info("Upload your dataset to begin")
-    st.stop()
-
-df = load_data(uploaded_file)
+df = load_data()
 
 if df.empty:
-    st.error("Dataset could not be loaded")
     st.stop()
 
-st.success(f"Dataset Loaded Successfully: {len(df)} rows")
+st.subheader("Dataset Preview")
+
 st.dataframe(df.head())
+
+st.divider()
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.plotly_chart(
+        adoption_trend_chart(df),
+        use_container_width=True
+    )
+
+with col2:
+    st.plotly_chart(
+        industry_chart(df),
+        use_container_width=True
+    )
+
+col3, col4 = st.columns(2)
+
+with col3:
+    st.plotly_chart(
+        country_chart(df),
+        use_container_width=True
+    )
+
+with col4:
+    st.plotly_chart(
+        investment_vs_revenue_chart(df),
+        use_container_width=True
+    )
+
+st.divider()
+
+st.subheader("Insights")
+
+for insight in generate_insights(df):
+    st.success(insight)
